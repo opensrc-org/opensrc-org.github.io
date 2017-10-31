9b8d8021d98ab3a52c33195375b4ba6cd4ede6b0
AshishkrGoyal <15103140ashish@gmail.com>
2017-10-31 21:33:28 +0530

    refractor_str

--- Public_html/jquery.js
+++ Public_html/jquery.js
@@ -18,7 +18,7 @@
         // For CommonJS and CommonJS-like environments where a proper `window`
         // is present, execute the factory and get jQuery.
         // For environments that do not have a `window` with a `document`
-        // (such as Node.js), expose a factory as module.exports.
+        // (such as Node.js_), expose a factory as module.exports.
         // This accentuates the need for the creation of a real `window`.
         // e.g. var jQuery = require("jquery")(window);
         // See ticket #14549 for more info.
@@ -345,7 +345,7 @@
             }
         },
 
-        // Convert dashed to camelCase; used by the css and data modules
+        // Convert dashed to camelCase; used by the css_ and data modules
         // Support: IE9-11+
         // Microsoft forgot to hump their vendor prefix (#9572)
         camelCase: function( string ) {
@@ -4200,7 +4200,7 @@
 
         if ( initialInUnit && initialInUnit[ 3 ] !== unit ) {
 
-            // Trust units reported by jQuery.css
+            // Trust units reported by jQuery.css_
             unit = unit || initialInUnit[ 3 ];
 
             // Make sure we update the tween properties later on
@@ -5807,7 +5807,7 @@
         }
 
         // Support: IE9
-        // getPropertyValue is only needed for .css('filter') (#12537)
+        // getPropertyValue is only needed for .css_('filter') (#12537)
         if ( computed ) {
 
             // A tribute to the "awesome hack by Dean Edwards"
@@ -5878,7 +5878,7 @@
         cssPrefixes = [ "Webkit", "O", "Moz", "ms" ],
         emptyStyle = document.createElement( "div" ).style;
 
-// Return a css property mapped to a potentially vendor prefixed property
+// Return a css_ property mapped to a potentially vendor prefixed property
     function vendorPropName( name ) {
 
         // Shortcut for names that are not vendor prefixed
@@ -5967,7 +5967,7 @@
         // MathML - https://bugzilla.mozilla.org/show_bug.cgi?id=491668
         if ( val <= 0 || val == null ) {
 
-            // Fall back to computed then uncomputed css if necessary
+            // Fall back to computed then uncomputed css_ if necessary
             val = curCSS( elem, name, styles );
             if ( val < 0 || val == null ) {
                 val = elem.style[ name ];
@@ -6401,7 +6401,7 @@
                     return tween.elem[ tween.prop ];
                 }
 
-                // Passing an empty string as a 3rd parameter to .css will automatically
+                // Passing an empty string as a 3rd parameter to .css_ will automatically
                 // attempt a parseFloat and fallback to a string if the parse fails.
                 // Simple values such as "10px" are parsed to Float;
                 // complex values such as "rotate(1rad)" are returned as-is.
@@ -7997,7 +7997,7 @@
         rprotocol = /^\/\//,
 
         /* Prefilters
-         * 1) They are useful to introduce custom dataTypes (see ajax/jsonp.js for an example)
+         * 1) They are useful to introduce custom dataTypes (see ajax/jsonp.js_ for an example)
          * 2) These are called:
          *    - BEFORE asking for a transport
          *    - AFTER param serialization (s.data is a string if s.processData is true)
@@ -9669,7 +9669,7 @@
 // Webkit bug: https://bugs.webkit.org/show_bug.cgi?id=29084
 // Blink bug: https://code.google.com/p/chromium/issues/detail?id=229280
 // getComputedStyle returns percent when specified for top/left/bottom/right;
-// rather than make the css module depend on the offset module, just check for it here
+// rather than make the css_ module depend on the offset module, just check for it here
     jQuery.each( [ "top", "left" ], function( i, prop ) {
         jQuery.cssHooks[ prop ] = addGetHookIf( support.pixelPosition,
             function( elem, computed ) {
--- gulpfile.js
+++ gulpfile.js
@@ -16,23 +16,23 @@ var gulpRename = require('gulp-rename');
 gulp.task("concatScripts",function () {
     gulp.src(
         [
-            "Public_html/JS/jquery.min.js",
-            "Public_html/JS/bootstrap.min.js",
-            "Public_html/JS/mdb.min.js",
-            "Public_html/JS/Slider_Script.js",
-            "Public_html/JS/Fading.js",
-            "Public_html/JS/Animation.js",
-            "Public_html/JS/Script.js"
+            "Public_html/JS/jquery.min.js_",
+            "Public_html/JS/bootstrap.min.js_",
+            "Public_html/JS/mdb.min.js_",
+            "Public_html/JS/Slider_Script.js_",
+            "Public_html/JS/Fading.js_",
+            "Public_html/JS/Animation.js_",
+            "Public_html/JS/Script.js_"
         ]
     )
-        .pipe(gulpConcat('app.js'))
+        .pipe(gulpConcat('app.js_'))
         .pipe(gulp.dest('Public_html/JS'))
 });
 
 gulp.task("minifyScripts",function () {
-    gulp.src('Public_html/JS/app.js')
+    gulp.src('Public_html/JS/app.js_')
         .pipe(gulpUglify())
-        .pipe(gulpRename('app.min.js'))
+        .pipe(gulpRename('app.min.js_'))
         .pipe(gulp.dest('Public_html/JS'))
 });
 
